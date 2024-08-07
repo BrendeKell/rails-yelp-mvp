da321be9f47cacd6b139712b0dc13420a8929087
Joaquim Zapater joaquim.zapater@audencia.com <joaquim.zapater@audencia.com>
2024-08-07 15:48:13 +0200

    exo2 ended

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,10 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -21,25 +21,25 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(params_restaurant)
     redirect_to @restaurant, notice: "restaurant was successfully updated!"
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path, status: :see_other
   end
-
 
   private
 
   def params_restaurant
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,13 @@ Restaurant.destroy_all
 puts "creating restaurants..."
 
 musa_dwich_club = {name: "Musa Dwich Club", address: "55 rue Jean-Jaurès 92170 Vanves", phone_number:"0123456789", category: "italian"}
-sushi_tokyo = { name: "Sushi Tokyo", address: "456 Cherry Blossom Avenue, Tokyo", phone_number: "0987654321", category: "japanese" },
-pasta_fresca ={ name: "Pasta Fresca", address: "789 Pasta Street, Rome", phone_number: "0123456789", category: "french" },
-waffles_and_more = { name: "Waffles & More", address: "321 Belgian Delight Lane, Brussels", phone_number: "0134567890", category: "belgian" },
+sushi_tokyo = { name: "Sushi Tokyo", address: "456 Cherry Blossom Avenue, Tokyo", phone_number: "0987654321", category: "japanese" }
+pasta_fresca ={ name: "Pasta Fresca", address: "789 Pasta Street, Rome", phone_number: "0123456789", category: "french" }
+waffles_and_more = { name: "Waffles & More", address: "321 Belgian Delight Lane, Brussels", phone_number: "0134567890", category: "belgian" }
 dragon_palace = { name: "Dragon Palace", address: "654 Dragon Road, Beijing", phone_number: "0145678901", category: "chinese" }
 
 [musa_dwich_club, sushi_tokyo, pasta_fresca, waffles_and_more, dragon_palace].each do |resto|
-  Restaurant.create!(resto)
-# puts "Created #{restaurant.name}"
+  rest = Restaurant.create!(resto)
+  puts "Created #{rest.name}"
 end
 puts "finished!"
